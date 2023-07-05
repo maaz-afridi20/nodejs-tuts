a006fbdddae9e83ff79fa3a8bf97f1036a868720
maaz-afridi20 <maazafridi.ma47@gmail.com>
2023-07-05 16:19:04 +0500

    new changes

---
+++ 5_file.js
@@ -0,0 +1,21 @@
+// node js is single threaded. aik hi time mein aik hi cheez ko run krti hai..
+
+
+// importing colors package.
+var colors = require('colors');
+
+console.log('this is rainbow'.rainbow); // this will print the text rang ba rang.
+console.log('this is green'.green);
+console.log('this is pata nai'.blue);
+
+// what are package.json file?
+
+// mainly ye jo file hoti hai ye huamri project ki details raktii hai.. 
+// alll related to code. it's like pubspes.yaml
+// like what is the git repo what is the verison etx.
+
+// we can create the package.json file through termina... through
+// npm init
+
+
+//
